b7f2ee9700d71c802135dfe2b88d206ae0d191c3
Arseny Shishaev <arseny.shishaev@gmail.com>
2019-12-14 21:41:08 +0100

    Add form.html

--- server/web.py
+++ server/web.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hansel alarm button'
+
+@app.route('/form/')
+def hello(name=None):
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run()
