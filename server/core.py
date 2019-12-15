c0bea26fa0944c9829f8b4b75fff735e4c565370
Balaram Usov <dest@disroot.org>
2019-12-15 18:21:12 +0300

    Added function for subscribing

--- server/core.py
+++ server/core.py
@@ -11,7 +11,17 @@ def register_user(name, number, contacts):
     shared_token  = secrets.token_hex(4)
     d['private'] = private_token
     d['shared'] = shared_token
+    d['subscribers'] = list()
     users_lock.acquire()
     users.append(d)
     users_lock.release()
     return private_token, shared_token
+
+def subscribe_to_token(shared_token, user_id):
+    users_lock.acquire()
+    for ud in users:
+        if ud['shared'] == shared_token:
+            ud['subscribers'].append(user_id)
+            return
+    else:
+        raise IndexError
