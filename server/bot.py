35d1cbccfc93150f661c8312e9349a8de5c41a5f
Nataliya Tyshkevich <natasha@api.ai>
2019-12-15 18:25:46 +0300

    Рабочий прототип бота для подписок на оповещения

--- server/bot.py
+++ server/bot.py
@@ -1,5 +1,5 @@
 # создаем телеграм бота
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from telegram import Bot, ReplyKeyboardMarkup, KeyboardButton
 from telegram.utils.request import Request
 
@@ -18,11 +18,32 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # приветственное сообщение
 def hello(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hello! My name is {0}".format(config.name))
+                             text="Привет! Через этого бота вы можете подписаться на срочные уведомления о том, что ваш друг попал в беду. Для этого введите команду /subscribe, а затем введите идентификатор этого человека. Если этот человек нажмет на свою кнопку тревоги, вам придет срочное оповещение.".format(config.name))
+
+def subscribe(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Введите личный идентификатор человека, на оповещения которого вы хотите подписаться.")
+    return "token"
+    
+def parse_token(update, context):
+
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Спасибо! Теперь вы подписаны на этого человека. Если с ним что-то случится, вам придет сообщение и его координаты.")
+    return ConversationHandler.END
 
 # добавляем приветственное сообщение при команде старт
 
+
 dp.add_handler(CommandHandler('start', hello))
+dp.add_handler(ConversationHandler(
+        entry_points=[CommandHandler('subscribe', subscribe)],
+
+        states={
+            "token": [MessageHandler(Filters.text, parse_token, pass_user_data=True)]
+        },
+        
+        fallbacks = []
+    ))
 
 def main():
     upd.start_polling()
